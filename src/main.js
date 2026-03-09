5c94409d291ada6b7a05f82bdec44fd7f4f4fed9
AlbertSch8 <albert.schurrer@gmail.com>
2026-03-09 10:41:00 +0100

    zaklad dashboardu a nacitani statistik ufouna

---
+++ src/main.js
@@ -0,0 +1,11 @@
+﻿import { loadDashboard } from "./dashboard.js";
+
+async function showDashboard(userId) {
+    const alien = await loadDashboard(userId);
+
+    console.log("Jmeno:", alien.name);
+    console.log("Level:", alien.level);
+    console.log("HP:", alien.hp);
+    console.log("DMG:", alien.dmg);
+    console.log("Stamina:", alien.stamina);
+}
