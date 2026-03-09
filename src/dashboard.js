6a9293dca458a74efbded102c0ae90c18a81a4fd
AlbertSch8 <albert.schurrer@gmail.com>
2026-03-09 10:39:36 +0100

    Dashboard načtení hráče

---
+++ src/dashboard.js
@@ -0,0 +1,13 @@
+﻿import { db } from "./firebase.js";
+import { doc, getDoc } from "firebase/firestore";
+
+export async function loadDashboard(userId) {
+    const alienRef = doc(db, "aliens", userId);
+    const alienSnap = await getDoc(alienRef);
+
+    if (!alienSnap.exists()) {
+        throw new Error("Ufoun nenalezen");
+    }
+
+    return alienSnap.data();
+}
