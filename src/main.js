22aad7486ec7af6136ddb6f832c7023d74b07821
Alexandr <basseville2000@gmail.com>
2026-03-12 09:35:23 +0100

    začátek dashboard a login

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
-﻿import { loadDashboard } from "./dashboard.js";
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-async function showDashboard(userId) {
-    const alien = await loadDashboard(userId);
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-    console.log("Jmeno:", alien.name);
-    console.log("Level:", alien.level);
-    console.log("HP:", alien.hp);
-    console.log("DMG:", alien.dmg);
-    console.log("Stamina:", alien.stamina);
-}
+setupCounter(document.querySelector('#counter'))
